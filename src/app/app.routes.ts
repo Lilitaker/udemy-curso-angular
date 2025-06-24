60416dd362f181e2f7baa5bcd7a627a19ddec4fd
Liliana Vasquez <lilitakerloki@gmail.com>
2025-06-24 08:05:49 -0500

    Create hero page table exercise

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
+
 /* Aquí se definen las rutas de nuestra aplicación */
-export const routes: Routes = [{
-  path: '',
-  component: CounterPageComponent,
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CounterPageComponent,
+  },
+  {
+    path: 'hero',
+    component: HeroPageComponent,
+  }
+];
